574de9b2954d470a1d774636f2ce30214cc15976
Fawwaz Ivandra <ivandrafawwaz@gmail.com>
2022-03-26 23:57:08 +0700

    var-command

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var myElement = document.getElementById("button").onclick = function(){
     
-    // baris-1
+    // baris-1-input
         let inputMatkul1 = document.getElementById("matkul-1");
         let outputMatkul1 = document.getElementById("hasil-matkul1");
         outputMatkul1.innerHTML = inputMatkul1.value;
@@ -13,7 +13,7 @@ var myElement = document.getElementById("button").onclick = function(){
         let outputAngka1 = document.getElementById("hasil-angka1");
         outputAngka1.innerHTML = inputAngka1.value;
 
-    // baris-2
+    // baris-2-input
         let inputMatkul2 = document.getElementById("matkul-2");
         let outputMatkul2 = document.getElementById("hasil-matkul2");
         outputMatkul2.innerHTML = inputMatkul2.value;
@@ -26,7 +26,7 @@ var myElement = document.getElementById("button").onclick = function(){
         let outputAngka2 = document.getElementById("hasil-angka2");
         outputAngka2.innerHTML = inputAngka2.value;
 
-    // baris-3
+    // baris-3-input
         let inputMatkul3 = document.getElementById("matkul-3");
         let outputMatkul3 = document.getElementById("hasil-matkul3");
         outputMatkul3.innerHTML = inputMatkul3.value;
@@ -39,7 +39,7 @@ var myElement = document.getElementById("button").onclick = function(){
         let outputAngka3 = document.getElementById("hasil-angka3");
         outputAngka3.innerHTML = inputAngka3.value;
 
-    // baris-4
+    // baris-4-input
         let inputMatkul4 = document.getElementById("matkul-4");
         let outputMatkul4 = document.getElementById("hasil-matkul4");
         outputMatkul4.innerHTML = inputMatkul4.value;
@@ -52,7 +52,7 @@ var myElement = document.getElementById("button").onclick = function(){
         let outputAngka4 = document.getElementById("hasil-angka4");
         outputAngka4.innerHTML = inputAngka4.value;
 
-    // baris-5
+    // baris-5-input
         let inputMatkul5 = document.getElementById("matkul-5");
         let outputMatkul5 = document.getElementById("hasil-matkul5");
         outputMatkul5.innerHTML = inputMatkul5.value;
@@ -66,22 +66,12 @@ var myElement = document.getElementById("button").onclick = function(){
         outputAngka5.innerHTML = inputAngka5.value;
 
     // Hasil IPK
-        let out_ip = document.getElementById('ip');
-        out_ip.innerHTML = ((+inputAngka1.value*inputSks1.value + +inputAngka2.value*inputSks2.value + +inputAngka3.value*inputSks3.value + +inputAngka4.value*inputSks4.value + +inputAngka5.value*inputSks1.value) 
+        let outputIp = document.getElementById('totalIp');
+        outputIp.innerHTML = ((+inputAngka1.value*inputSks1.value + +inputAngka2.value*inputSks2.value + +inputAngka3.value*inputSks3.value + +inputAngka4.value*inputSks4.value + +inputAngka5.value*inputSks1.value) 
         / (+inputSks1.value + +inputSks2.value + +inputSks3.value + +inputSks4.value + +inputSks5.value)).toFixed(2);
-        
-        if(inputAngka1.value == 4){
-            document.getElementById("huruf1").innerHTML= "A";
-        }else if(inputAngka1.value == 3){
-            document.getElementById("huruf1").innerHTML="B";
-        }else if(inputAngka1.value == 2){
-            document.getElementById("huruf1").innerHTML="C";
-        }else if(inputAngka1.value == 1){
-            document.getElementById("huruf1").innerHTML="D";
-        }else if(inputAngka1.value == 0){
-            document.getElementById("huruf1").innerHTML="E";
-        }
 
+    // Nilai Huruf    
+        
         if(inputAngka1.value == 4){
             document.getElementById("huruf1").innerHTML= "A";
         }else if(inputAngka1.value == 3){
